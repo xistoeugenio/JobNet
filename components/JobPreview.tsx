1ac1c6604fd7187b846df142851634fe4e088402
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-04-02 15:13:38 -0300

    style: Update styles for JobPreview component

--- components/JobPreview.tsx
+++ components/JobPreview.tsx
@@ -1,20 +1,29 @@
-import { RiDeleteBin7Line } from "react-icons/ri";
+import { BiLinkExternal } from "react-icons/bi";
 
 interface JobPreviewProps {
-  name: string;
+  title: string;
+  company_name: string;
+  current_status: string;
 }
 
-const JobPreview: React.FC<JobPreviewProps> = ({ name }) => {
+const JobPreview: React.FC<JobPreviewProps> = ({
+  title,
+  company_name,
+  current_status,
+}) => {
   return (
-    <div
-      className="
-     bg-neutral-800 h-10
-     flex justify-between items-center
-     box-border px-6 m-3 
-     rounded-xl"
-    >
-      <div>{name}</div>
-      <RiDeleteBin7Line />
+    <div className="bg-neutral-800 h-10 flex justify-between items-center box-border px-3 m-3 rounded-xl">
+      <span className="bg-emerald-900 w-1/3 sm:w-1/4 text-neutral-300 rounded-md px-1 overflow-hidden whitespace-nowrap text-ellipsis text-sm sm:text-base">
+        {title}
+      </span>
+      <span className="w-1/5 text-cyan-600 sm:text-lg overflow-hidden whitespace-nowrap text-ellipsis">
+        {company_name}
+      </span>
+      <span className="w-1/4 text-neutral-400 text-xs sm:text-sm">
+        {current_status}
+      </span>
+      <span className="text-neutral-500 hidden sm:block">3 hours</span>
+      <BiLinkExternal className="text-red-500" />
     </div>
   );
 };
--- components/listJobs/ListJobs.tsx
+++ components/listJobs/ListJobs.tsx
@@ -5,7 +5,12 @@ const ListJobs = () => {
   return (
     <div className=" bg-neutral-950 flex-1 rounded-b-md">
       {jobs_applied.map((job, index) => (
-        <JobPreview key={index} name={job.Title} />
+        <JobPreview 
+        key={index} 
+        title={job.Title} 
+        company_name={job.company_name}
+        current_status={job.current_status}
+        />
       ))}
     </div>
   );
