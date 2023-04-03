73afeefd0d1e29220ad703fa058ea746a74a4b9b
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-04-03 11:29:43 -0300

    feat: Add Button and Modal components

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AvatarName from "../AvatarName";
+import Button from "../Button";
 
 const Header = () => {
   return (
     <div
       className=" 
-    h-14
+    h-16
     flex justify-between items-center
     box-border px-5 
     border-b-2 border-gray-500
