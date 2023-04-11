735eb29a96cd428a6dc3c819f483abefbb81d287
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-04-10 22:53:37 -0300

    feat: add HomePage component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,14 +6,16 @@ import { signOut } from "next-auth/react";
 import useLoginModal from "@/hooks/useLoginModal";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
+import useRegisterModal from "@/hooks/useRegisterModal";
 
 const Header = () => {
   const { data: currentUser } = useCurrentUser();
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
   const router = useRouter();
 
   const goToAddPage = useCallback(() => {
-      router.push("/add")
+    router.push("/add");
   }, [router]);
 
   return (
@@ -30,11 +32,18 @@ const Header = () => {
           <AvatarName username={currentUser?.username} />
           <div className="flex gap-3">
             <Button label="Sign out" secondary onClick={signOut} />
-            <Button label="Add a job" outline onClick={goToAddPage}/>
+            <Button label="Add a job" outline onClick={goToAddPage} />
           </div>
         </>
       ) : (
-        <Button label="Login" outline onClick={loginModal.onOpen} />
+        <>
+          <Button
+            label="Join to jobNet"
+            onClick={registerModal.onOpen}
+            outline
+          />
+          <Button label="Login" onClick={loginModal.onOpen} />
+        </>
       )}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/header/Header";
 import styles from "../styles/home.module.scss";
 import ListJobs from "@/components/listJobs/ListJobs";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
+  const { data: currentUser, isLoading } = useCurrentUser();
   return (
     <div className={styles.homeContainer}>
       <Header />
-      <ListJobs />
+      {currentUser ? <ListJobs /> : <HomePage />}
     </div>
   );
 }
